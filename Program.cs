174e4152db0f3723f37ea62f2e1e686ea83b0a81
intisar said <107991431+intisar996@users.noreply.github.com>
2023-05-21 19:20:45 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,63 +1,16 @@
-﻿namespace ConsoleApp1
+﻿namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            caterpiller cat = new caterpiller(1, "Honda", "black car");
+               cat.move();
+               cat.stop();
+               cat.unload();
+               cat.load();
 
-            var Wel = "Welcome To My First Website"; // This Wel varible
-            Console.WriteLine("-------------------" + Wel + "-------------------------");
-            Console.WriteLine("--------------------------------------------------------------------");
-
-            //  Concat string
-            String MyName = "Intisar Said";
-            int MyAge = 27;
-            Console.WriteLine("My Name IS : " + MyName + " My Age :" + MyAge);
-            Console.WriteLine("--------------------------------------------------------------------");
-
-            // && , || , !=
-
-            int a = 2;
-            int b = 3;
-            int c = 4;
-
-            Console.WriteLine((a > b) && (c > b));
-            Console.WriteLine((a == b) || (c == b));
-            Console.WriteLine((a != b) || (c == b));
-            Console.WriteLine("--------------------------------------------------------------------");
-
-            // sum 
-            int x = 4, y = 5;
-            Console.WriteLine(x + y);
-
-            //
-            Console.WriteLine(x.GetHashCode());
-
-            Object bx = new Object();
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+           
 
 
         }
---
+++ Vehicle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Vehicle
+    {
+        prop model;
+        prop year;
+        prop Category;
+
+        public Vehicle(model,year,Category)
+        {
+            this.model = model;
+            this.year = year;
+            this.Category = Category;
+        }
+    }
+
+    interface IDrivable
+    {
+        void move();
+        void stop();
+        }
+     interface Iloader
+    {
+        void load();
+        void unload();
+    }
+
+    class caterpiller : Vehicle, IDrivable, Iloader
+    {
+        public caterpiller(model,year,Category) : base(model,year,Category)
+        {
+
+        }
+
+        public void load()
+        {
+            Console.WriteLine("loading");
+        }
+
+        public void move() => Console.WriteLine("moveing");
+       
+
+        public void stop() => Console.WriteLine("stoping");
+      
+
+        public void unload() =>Console.WriteLine("unloading");
+       
+    }
+}
