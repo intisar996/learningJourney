65f6b351c10b50ecf71fc20f3d389c806d592ce0
intisar said <107991431+intisar996@users.noreply.github.com>
2023-05-21 18:03:41 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Wel = "Welcome To My First Website"; // This Wel varible
+            Console.WriteLine("-------------------" + Wel + "-------------------------");
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            //  Concat string
+            String MyName = "Intisar Said";
+            int MyAge = 27;
+            Console.WriteLine("My Name IS : " + MyName + " My Age :" + MyAge);
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            // && , || , !=
+
+            int a = 2;
+            int b = 3;
+            int c = 4;
+
+            Console.WriteLine((a > b) && (c > b));
+            Console.WriteLine((a == b) || (c == b));
+            Console.WriteLine((a != b) || (c == b));
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            // sum 
+            int x = 4, y = 5;
+            Console.WriteLine(x + y);
+
+            //
+            Console.WriteLine(x.GetHashCode());
+
+            Object bx = new Object();
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
