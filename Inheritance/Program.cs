f49da2a6cb6ee891167f36c8046a6ffe119996be
intisar said <107991431+intisar996@users.noreply.github.com>
2023-05-21 18:10:12 +0400

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //cat class 
+            Console.WriteLine("---cat class---");
+            Cat cat = new Cat();
+            cat.name = "tome";
+            cat.age = 2;
+            cat.gender = "Female";
+            Console.WriteLine(cat.name);
+            Console.WriteLine(cat.age);
+            Console.WriteLine(cat.gender);
+            cat.move();
+            cat.soud();
+            cat.makeSound();
+            Console.WriteLine(cat);//show the override mothed  ToString
+
+            // dog calss
+            Console.WriteLine("---dog class---");
+            Dog dog = new Dog();
+            dog.name = "Sam";
+            dog.age = 4;
+            dog.category = "dog";
+            Console.WriteLine(dog.name);
+            Console.WriteLine(dog.age);
+            Console.WriteLine(dog.category);
+            dog.move();
+            dog.soud();
+            dog.makeSound();
+
+            //Eagle class
+            Console.WriteLine("---eagle class---");
+            Eagle eagle = new Eagle();
+            eagle.name = "sd";
+            eagle.age = 2;
+            eagle.gender = "Male";
+            Console.WriteLine(eagle.name);
+            Console.WriteLine(eagle.age);
+            Console.WriteLine(eagle.gender);
+            eagle.move();
+            eagle.flay();
+            eagle.makeSound();
+
+            //Vehicle 
+            honda ho = new honda("Mazda",12,"Car red",2016);//because it has constractor need to give it value
+        }
+    }
+}
