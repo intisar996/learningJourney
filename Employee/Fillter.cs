ba29ba48e66219967967d9b04b5a099920d3f8f3
intisar said <107991431+intisar996@users.noreply.github.com>
2023-05-29 10:42:07 +0400

    Add files via upload

---
+++ Employee/Fillter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Fillter
+    {
+     public  delegate  bool Fillteration(Employee e);
+
+
+     public void EmpInfo(Employee[] emp,string Title, Fillteration fillter)
+        {
+
+
+
+
+            Console.WriteLine(Title);
+            Console.WriteLine("**********************");
+            Console.WriteLine("Name | Age | Salary | Totalsale");
+
+
+            foreach (Employee e in emp)
+            {
+              if(fillter(e))
+                {
+
+                    Console.WriteLine($"{e.Name} | {e.age} | {e.salary} | {e.Totalsale}");
+                }
+            }
+            Console.WriteLine("\n\n");
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Employee/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] em = new Employee[]
+
+            {
+
+
+            new Employee { Name = "intisar", age = 27, salary = 2000, Totalsale = 100000m},
+            new Employee { Name = "Fatma", age = 23, salary = 350, Totalsale = 200m },
+            new Employee { Name = "Safa", age = 28, salary = 300, Totalsale = 300m },
+            new Employee { Name = "Noor", age = 24, salary = 400, Totalsale = 1000m },
+            new Employee { Name = "bayan", age = 20, salary = 500, Totalsale = 3000m },
+
+        };
+           
+
+       
+
+
+
+            Fillter f = new Fillter();
+            
+
+
+            f.EmpInfo(em,"Employee Age Less Than 25", (emp)=> emp.age < 25);
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
