02f4f017f706aa4fbf2ca25ff8948331ee70f281
intisar said <107991431+intisar996@users.noreply.github.com>
2023-05-29 10:46:46 +0400

    Add files via upload

---
+++ Animal/ConsoleApp3/Animal.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    abstract class Animal // superclass 
+    {
+        public string name;
+        public int age;
+
+        public virtual void move()
+        {
+            Console.WriteLine($"Animal name {name} " + $"age : {age}");
+
+
+        }
+        public abstract void MakeSound();
+
+    }
+
+    class Cat : Animal // subclass
+    {
+        public string gender;
+        
+        public override void move()
+        {
+            base.move();
+            Console.WriteLine("xxx zzzz xx ");
+        }
+        public void sound()
+        {
+            
+            Console.WriteLine("Meow");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("meeow meowee ");
+        }
+
+        public override string ToString()
+        {
+            return $"my name is {this.name}";
+        }
+
+    }
+
+         class   Dog : Animal // subclass
+    {
+            public string gender;
+
+            public void sound()
+            {
+
+                Console.WriteLine("woof");
+            }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("wooof woooeef ");
+        }
+    }
+
+
+         class Egale : Animal // subclass
+    {
+        public string gender;
+
+        public void sound()
+        {
+
+            Console.WriteLine("woof");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("wooof woooeef ");
+        }
+    }
+
+
+
+}
+
+
---
+++ Animal/ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+
+            d.sound();
+             d.name = "bobbe";
+             d.age = 1;
+            Console.WriteLine($"{d.name} {d.age}");
+            d.move();
+
+
+            Cat c = new Cat();
+             c.name = "lulu";
+              c.age = 2;
+            Console.WriteLine($"{c.name} {c.age}");
+            c.sound();
+            c.move();
+            Console.WriteLine(c);
+
+
+
+        }
+    }
+}
